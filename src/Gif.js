9b30bda0d0eb67182205db5e56b9b4be8a2b0908
mahouhou <frejasmith@frejas-mbp.home>
2022-07-19 13:09:30 +0100

    Update to React 5.0.1. Minor edits to comments.

--- src/Gif.js
+++ src/Gif.js
@@ -10,17 +10,23 @@ class Gif extends Component {
 	}
 
 	render() {
+		//images is an object containing the props object for Gif ??
 		const {images} = this.props;
+		//loaded contains the state for Gif ??
 		const {loaded} = this.state;
 		return(
 			<video
 				//When the gif has loaded, we give it class 'loaded'
 				//We use CSS to style the transition
+				//className includes all state as classes plus 'loaded' when loaded ??
+				//If I remove "loaded &&", the app still works ??
+				//If I remove "&& 'loaded'", it stops loading gifs ??
 				className={`grid-item video ${loaded && 'loaded'}`}
 				autoPlay
 				loop
 				src={images.original.mp4}
-				//This checks if the gif has loaded yet and sets the state to true when it has
+				//This checks if the gif has loaded yet
+				//and sets the state to true when it has
 				onLoadedData={() => this.setState({loaded: true})}
 			/>
 			)
